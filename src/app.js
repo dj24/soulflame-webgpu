56b0d447ff2bfdc49f69172792b0e45b33ce3948
Daniel Jackson <danieljackson97123@gmail.com>
2023-11-14 20:29:29 +0000

    wip

--- src/app.js
+++ src/app.js
@@ -9,12 +9,24 @@ class RenderLoop {
     context;
     bindGroup;
     computeBindGroup;
-    outputTexture;
+    outputTextureView;
     constructor() {
         this.start().then(() => {
             requestAnimationFrame(this.frame.bind(this));
         });
     }
+
+    get timeBuffer() {
+        const timeBuffer = this.device.createBuffer({
+            size: 4,
+            mappedAtCreation: true,
+            usage: GPUBufferUsage.UNIFORM,
+        });
+        new Uint32Array(timeBuffer.getMappedRange())[0] = new Date().getTime();
+        timeBuffer.unmap();
+        return timeBuffer;
+    }
+
     async start(){
         if (navigator.gpu === undefined) {
             throw new Error('WebGPU not supported');
@@ -24,7 +36,7 @@ class RenderLoop {
         this.device = await adapter.requestDevice();
 
         this.shaderModule = this.device.createShaderModule({code: shaderCode});
-        
+
         // Handle compilation errors
         var compilationInfo = await this.shaderModule.getCompilationInfo();
         if (compilationInfo.messages.length > 0) {
@@ -39,7 +51,7 @@ class RenderLoop {
                 throw new Error("Shader failed to compile");
             }
         }
-        
+
         // Get a context to display our rendered image on the canvas
         var canvas = document.getElementById("webgpu-canvas");
         canvas.width = canvas.parentElement.clientWidth;
@@ -51,43 +63,6 @@ class RenderLoop {
         this.context.configure(
             {device: this.device, format: presentationFormat, usage: GPUTextureUsage.RENDER_ATTACHMENT});
 
-        const response = await fetch('./paris.jpg');
-        const imageBitmap = await createImageBitmap(await response.blob());
-
-        const {width, height} = imageBitmap;
-        this.outputTexture = this.device.createTexture({
-            size: [width, width, 1],
-            format: 'rgba8unorm',
-            usage:
-                GPUTextureUsage.TEXTURE_BINDING |
-                GPUTextureUsage.COPY_DST |
-                GPUTextureUsage.RENDER_ATTACHMENT |
-                GPUTextureUsage.STORAGE_BINDING
-        });
-        
-        this.device.queue.copyExternalImageToTexture(
-            { source: imageBitmap },
-            { texture: this.outputTexture },
-            [width, height]
-        );
-
-        const bindGroupLayout = this.device.createBindGroupLayout({
-            entries: [{
-                binding: 0, // texture sampler
-                visibility: GPUShaderStage.FRAGMENT,
-                sampler: {},
-            }, {
-                binding: 1, // model uniform
-                visibility: GPUShaderStage.FRAGMENT,
-                texture: {},
-            }]
-        });
-
-        const pipelineLayout = this.device.createPipelineLayout({
-            bindGroupLayouts: [
-                bindGroupLayout, // @group(0)
-            ]
-        });
 
         // Create compute pipeline
         this.computePipeline = this.device.createComputePipeline({
@@ -102,7 +77,7 @@ class RenderLoop {
 
         // Create render pipeline
         this.renderPipeline = this.device.createRenderPipeline({
-            layout: pipelineLayout,
+            layout: 'auto',
             vertex: {
                 module: this.shaderModule,
                 entryPoint: "vertex_main",
@@ -114,64 +89,47 @@ class RenderLoop {
             },
         });
 
-        const sampler = this.device.createSampler({
-            magFilter: 'linear',
-            minFilter: 'linear',
-        });
-        
-        this.bindGroup = this.device.createBindGroup({
-            layout: this.renderPipeline.getBindGroupLayout(0),
-            entries: [
-                {
-                    binding: 0,
-                    resource: sampler,
-                },
-                {
-                    binding: 1,
-                    resource: this.outputTexture.createView(),
-                },
-            ],
+        const outputTexture = this.device.createTexture({
+            size: [512, 512, 1],
+            format: 'rgba8unorm',
+            usage:
+              GPUTextureUsage.TEXTURE_BINDING |
+              GPUTextureUsage.RENDER_ATTACHMENT |
+              GPUTextureUsage.STORAGE_BINDING
         });
 
-        
+        this.outputTextureView = outputTexture.createView();
+
     }
-    async frame() {
+     async frame() {
         const commandEncoder = this.device.createCommandEncoder();
 
         const computePass = commandEncoder.beginComputePass();
         computePass.setPipeline(this.computePipeline);
 
-        const timeBuffer = this.device.createBuffer({
-            size: 4,
-            mappedAtCreation: true,
-            usage: GPUBufferUsage.UNIFORM,
-        });
-        new Uint32Array(timeBuffer.getMappedRange())[0] = new Date().getTime();
-        timeBuffer.unmap();
-        
-        
+
         this.computeBindGroup = this.device.createBindGroup({
             layout: this.computePipeline.getBindGroupLayout(0),
             entries: [
                 {
                     binding: 0,
-                    resource: this.outputTexture.createView(),
+                    resource: this.outputTextureView,
                 },
                 {
                     binding: 1,
                     resource: {
-                        buffer: timeBuffer
+                        buffer: this.timeBuffer
                     }
                 }
             ],
         });
         computePass.setBindGroup(0, this.computeBindGroup);
         computePass.dispatchWorkgroups(
-            256,
-            256
+            64,
+            64
         );
         computePass.end();
-        
+
         const renderPass = commandEncoder.beginRenderPass({
             colorAttachments: [{
                 view: this.context.getCurrentTexture().createView(),
@@ -180,11 +138,36 @@ class RenderLoop {
                 storeOp: "store"
             }]
         });
+
+         this.bindGroup = this.device.createBindGroup({
+             layout: this.renderPipeline.getBindGroupLayout(0),
+             entries: [
+                 {
+                     binding: 0,
+                     resource: this.device.createSampler({
+                         magFilter: 'linear',
+                         minFilter: 'linear',
+                     }),
+                 },
+                 {
+                     binding: 1,
+                     resource: this.outputTextureView,
+                 },
+                 {
+                     binding: 2,
+                     resource: {
+                         buffer: this.timeBuffer
+                     }
+                 }
+             ],
+         });
+
         renderPass.setPipeline(this.renderPipeline);
         renderPass.setBindGroup(0, this.bindGroup);
         renderPass.draw(6);
         renderPass.end();
         this.device.queue.submit([commandEncoder.finish()]);
+
         requestAnimationFrame(this.frame.bind(this));
     };
 }
--- src/blur.wgsl
+++ src/blur.wgsl
@@ -1,10 +1,14 @@
 @group(0) @binding(0) var outputTex : texture_storage_2d<rgba8unorm, write>;
-@group(0) @binding(1) var<uniform> time : f32;
+@group(0) @binding(1) var<uniform> time : u32;
 
 @compute @workgroup_size(1, 1, 1)
 fn main(
   @builtin(workgroup_id) WorkGroupID : vec3<u32>,
   @builtin(local_invocation_id) LocalInvocationID : vec3<u32>
 ) {
-  textureStore(outputTex, WorkGroupID.xy, vec4(1,0,0,1));
+  var offset = sin(f32(time) * 0.001) * 256 + 256;
+  var red = sin(f32(time) * 0.001) * 0.5 + 0.5;
+  var blue = 1 - red;
+  var id = vec2<f32>(WorkGroupID.xy) + vec2(offset, 0);
+  textureStore(outputTex, vec2<u32>(id), vec4(red,0,blue,1));
 }
