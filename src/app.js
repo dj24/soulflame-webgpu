891ed11b7138dd838bc16760c231b682107707fd
Daniel Jackson <danieljackson97123@gmail.com>
2023-11-15 22:36:16 +0000

    add prettier and more shader stuff

--- src/app.js
+++ src/app.js
@@ -1,187 +1,295 @@
 import shaderCode from "./fullscreentexturedquad.wgsl";
 import blurWGSL from "./blur.wgsl";
 
+const createUniformBuffer = (items, label) => {
+  const uintArray = new Uint32Array(items);
+  const buffer = device.createBuffer({
+    size: uintArray.byteLength, // TODO: figure out why this needs to be 64
+    mappedAtCreation: true,
+    usage: GPUBufferUsage.UNIFORM,
+    label,
+  });
+  const mappedRange = new Uint32Array(buffer.getMappedRange());
+  items.forEach((item, index) => {
+    mappedRange[index] = item;
+  });
+  buffer.unmap();
+  return buffer;
+};
+
+const createFloatUniformBuffer = (items, label) => {
+  const floatArray = new Float32Array(items);
+  const buffer = device.createBuffer({
+    size: Math.max(64, floatArray.byteLength), // TODO: figure out why this needs to be 64
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(
+    buffer,
+    0, // offset
+    floatArray.buffer,
+    0, // data offset
+    items.length * Float32Array.BYTES_PER_ELEMENT,
+  );
+  return buffer;
+};
+
 class Vector2 {
-    x;
-    y;
-    constructor(x,y) {
-        this.x = x;
-        this.y = y;
-    }
+  x;
+  y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  get uniformBuffer() {
+    return createUniformBuffer([this.x, this.y]);
+  }
+
+  static zero = new Vector2(0, 0);
+  static one = new Vector2(0, 0);
+
+  toArray() {
+    return [this.x, this.y];
+  }
 }
- 
+
+class Vector3 {
+  x;
+  y;
+  z;
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  get uniformBuffer() {
+    return createUniformBuffer([this.x, this.y, this.z]);
+  }
+
+  static zero = new Vector3(0, 0, 0);
+  static one = new Vector3(0, 0, 0);
+
+  toArray() {
+    return [this.x, this.y, this.z];
+  }
+}
+
 let device;
-let resolution = new Vector2(0,0);
+let resolution = new Vector2(0, 0);
 const startTime = performance.now();
 
+const getFrustumCornerDirections = (camPos, fov) => {
+  const aspectRatio = resolution.x / resolution.y;
+
+  // Calculate half-height and half-width of the near plane
+  const halfHeight = Math.tan((fov / 2) * (Math.PI / 180));
+  const halfWidth = aspectRatio * halfHeight;
+
+  // Calculate directions to the corners of the near plane
+  const topLeft = new Vector3(
+    camPos.x - halfWidth,
+    camPos.y + halfHeight,
+    camPos.z - 1,
+  );
+  const topRight = new Vector3(
+    camPos.x + halfWidth,
+    camPos.y + halfHeight,
+    camPos.z - 1,
+  );
+  const bottomLeft = new Vector3(
+    camPos.x - halfWidth,
+    camPos.y - halfHeight,
+    camPos.z - 1,
+  );
+  const bottomRight = new Vector3(
+    camPos.x + halfWidth,
+    camPos.y - halfHeight,
+    camPos.z - 1,
+  );
+
+  // Return an array containing the directions to the corners
+  return [topLeft, topRight, bottomLeft, bottomRight];
+};
+
 const createComputePass = () => {
-    let computePipeline;
-    const start = () => {
-        computePipeline = device.createComputePipeline({
-            layout: 'auto',
-            compute: {
-                module: device.createShaderModule({
-                    code: blurWGSL,
-                }),
-                entryPoint: 'main',
-            },
-        });
-    }
-    const render = ({commandEncoder, timeBuffer, resolutionBuffer, outputTextureView}) => {
-        const computePass = commandEncoder.beginComputePass();
-        computePass.setPipeline(computePipeline);
-        const computeBindGroup = device.createBindGroup({
-            layout: computePipeline.getBindGroupLayout(0),
-            entries: [
-                {
-                    binding: 0,
-                    resource: outputTextureView,
-                },
-                {
-                    binding: 1,
-                    resource: {
-                        buffer: timeBuffer
-                    }
-                },
-                {
-                    binding: 2,
-                    resource: {
-                        buffer: resolutionBuffer
-                    }
-                }
-            ],
-        });
-        computePass.setBindGroup(0, computeBindGroup);
-        computePass.dispatchWorkgroups(
-            resolution.x,
-            resolution.y
-        );
-        computePass.end();
-    }
-    
-    return { start, render }
-}
+  let computePipeline;
+  const start = () => {
+    computePipeline = device.createComputePipeline({
+      layout: "auto",
+      compute: {
+        module: device.createShaderModule({
+          code: blurWGSL,
+        }),
+        entryPoint: "main",
+      },
+    });
+  };
+  const render = ({
+    commandEncoder,
+    timeBuffer,
+    resolutionBuffer,
+    outputTextureView,
+  }) => {
+    const flatMappedDirections = getFrustumCornerDirections(
+      new Vector3(0, 0, 5),
+      70,
+    ).flatMap((direction) => direction.toArray());
+    const frustumCornerDirectionsBuffer = createFloatUniformBuffer(
+      flatMappedDirections,
+      "frustum corner directions",
+    );
+    const computePass = commandEncoder.beginComputePass();
+    computePass.setPipeline(computePipeline);
+    const computeBindGroup = device.createBindGroup({
+      layout: computePipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: outputTextureView,
+        },
+        {
+          binding: 1,
+          resource: {
+            buffer: timeBuffer,
+          },
+        },
+        {
+          binding: 2,
+          resource: {
+            buffer: resolutionBuffer,
+          },
+        },
+        // {
+        //   binding: 3,
+        //   resource: {
+        //     buffer: frustumCornerDirectionsBuffer,
+        //   },
+        // },
+      ],
+    });
+    computePass.setBindGroup(0, computeBindGroup);
+    computePass.dispatchWorkgroups(resolution.x, resolution.y);
+    computePass.end();
+  };
+
+  return { start, render };
+};
 
 const renderLoop = (device, computePasses) => {
-    let bindGroup;
-    let outputTexture;
-    let animationFrameId;
+  let bindGroup;
+  let outputTexture;
+  let animationFrameId;
 
-    const canvas = document.getElementById("webgpu-canvas");
-    const context = canvas.getContext("webgpu");
-    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
-    context.configure({device: device, format: presentationFormat, usage: GPUTextureUsage.RENDER_ATTACHMENT});
-    const shaderModule = device.createShaderModule({code: shaderCode});
-    
-    const renderPipeline = device.createRenderPipeline({
-        layout: 'auto',
-        vertex: {
-            module: shaderModule,
-            entryPoint: "vertex_main",
-        },
-        fragment: {
-            module: shaderModule,
-            entryPoint: "fragment_main",
-            targets: [{format: presentationFormat}]
-        },
+  const canvas = document.getElementById("webgpu-canvas");
+  const context = canvas.getContext("webgpu");
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device: device,
+    format: presentationFormat,
+    usage: GPUTextureUsage.RENDER_ATTACHMENT,
+  });
+  const shaderModule = device.createShaderModule({ code: shaderCode });
+
+  const renderPipeline = device.createRenderPipeline({
+    layout: "auto",
+    vertex: {
+      module: shaderModule,
+      entryPoint: "vertex_main",
+    },
+    fragment: {
+      module: shaderModule,
+      entryPoint: "fragment_main",
+      targets: [{ format: presentationFormat }],
+    },
+  });
+  const start = () => {
+    const { clientWidth, clientHeight } = canvas.parentElement;
+    resolution = new Vector2(clientWidth, clientHeight);
+    canvas.width = resolution.x;
+    canvas.height = resolution.y;
+
+    computePasses.forEach((computePass) => {
+      computePass.start();
     });
-    const start = () => {
-        const { clientWidth, clientHeight} = canvas.parentElement;
-        resolution = new Vector2(clientWidth, clientHeight);
-        canvas.width = resolution.x;
-        canvas.height = resolution.y;
-        
-        
-        computePasses.forEach(computePass => {
-            computePass.start();
-        })
-        
-        animationFrameId = requestAnimationFrame(frame);
-    }
-     const frame = async () => {
-        const commandEncoder = device.createCommandEncoder();
-        
-        const timeBuffer = device.createBuffer({
-             size: 4,
-             mappedAtCreation: true,
-             usage: GPUBufferUsage.UNIFORM,
-        });
-        new Uint32Array(timeBuffer.getMappedRange())[0] = performance.now() - startTime;
-        timeBuffer.unmap();
 
-         const resolutionBuffer = device.createBuffer({
-             size: 8,
-             mappedAtCreation: true,
-             usage: GPUBufferUsage.UNIFORM,
-         });
-         const foo = new Uint32Array(resolutionBuffer.getMappedRange());
-         foo[0] = resolution.x;
-         foo[1] = resolution.y;
-         resolutionBuffer.unmap();
-         
-         outputTexture = device.createTexture({
-             size: [resolution.x, resolution.y, 1],
-             format: 'rgba8unorm',
-             usage:
-                 GPUTextureUsage.TEXTURE_BINDING |
-                 GPUTextureUsage.RENDER_ATTACHMENT |
-                 GPUTextureUsage.STORAGE_BINDING
-         });
-        const outputTextureView = outputTexture.createView();
-        
-        computePasses.forEach(computePass => {
-            computePass.render({commandEncoder, timeBuffer, resolutionBuffer, outputTextureView});
-        })
-        
-        const renderPass = commandEncoder.beginRenderPass({
-            colorAttachments: [{
-                view: context.getCurrentTexture().createView(),
-                loadOp: "clear",
-                clearValue: [0.3, 0.3, 0.3, 1],
-                storeOp: "store"
-            }]
-        });
+    animationFrameId = requestAnimationFrame(frame);
+  };
+  const frame = async () => {
+    const commandEncoder = device.createCommandEncoder();
+    const timeBuffer = createUniformBuffer([performance.now() - startTime]);
+    const resolutionBuffer = createUniformBuffer([resolution.x, resolution.y]);
 
-         bindGroup = device.createBindGroup({
-             layout: renderPipeline.getBindGroupLayout(0),
-             entries: [
-                 {
-                     binding: 0,
-                     resource: device.createSampler({
-                         magFilter: 'linear',
-                         minFilter: 'linear',
-                     }),
-                 },
-                 {
-                     binding: 1,
-                     resource: outputTextureView,
-                 },
-             ],
-         });
-        renderPass.setPipeline(renderPipeline);
-        renderPass.setBindGroup(0, bindGroup);
-        renderPass.draw(6);
-        renderPass.end();
-        device.queue.submit([commandEncoder.finish()]);
-        animationFrameId = requestAnimationFrame(frame);
-    };
-    
-    const resizeObserver = new ResizeObserver(() => {
-        cancelAnimationFrame(animationFrameId);
-        start();
+    outputTexture = device.createTexture({
+      size: [resolution.x, resolution.y, 1],
+      format: "rgba8unorm",
+      usage:
+        GPUTextureUsage.TEXTURE_BINDING |
+        GPUTextureUsage.RENDER_ATTACHMENT |
+        GPUTextureUsage.STORAGE_BINDING,
     });
-    resizeObserver.observe(canvas.parentElement)
-}
+    const outputTextureView = outputTexture.createView();
+
+    computePasses.forEach((computePass) => {
+      computePass.render({
+        commandEncoder,
+        timeBuffer,
+        resolutionBuffer,
+        outputTextureView,
+      });
+    });
+
+    const renderPass = commandEncoder.beginRenderPass({
+      colorAttachments: [
+        {
+          view: context.getCurrentTexture().createView(),
+          loadOp: "clear",
+          clearValue: [0.3, 0.3, 0.3, 1],
+          storeOp: "store",
+        },
+      ],
+    });
+
+    bindGroup = device.createBindGroup({
+      layout: renderPipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: device.createSampler({
+            magFilter: "linear",
+            minFilter: "linear",
+          }),
+        },
+        {
+          binding: 1,
+          resource: outputTextureView,
+        },
+      ],
+    });
+    renderPass.setPipeline(renderPipeline);
+    renderPass.setBindGroup(0, bindGroup);
+    renderPass.draw(6);
+    renderPass.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+    animationFrameId = requestAnimationFrame(frame);
+  };
+
+  const resizeObserver = new ResizeObserver(() => {
+    cancelAnimationFrame(animationFrameId);
+    start();
+  });
+  resizeObserver.observe(canvas.parentElement);
+};
 
 if (navigator.gpu !== undefined) {
-    navigator.gpu.requestAdapter().then(adapter => {
-        adapter.requestDevice().then(newDevice => {
-            device = newDevice;
-            const computePass = createComputePass();
-            renderLoop(device, [computePass]);
-        })
-    })
-} else{
-   console.error('WebGPU not supported');
+  navigator.gpu.requestAdapter().then((adapter) => {
+    adapter.requestDevice().then((newDevice) => {
+      device = newDevice;
+      const computePass = createComputePass();
+      renderLoop(device, [computePass]);
+    });
+  });
+} else {
+  console.error("WebGPU not supported");
 }
-
--- src/blur.wgsl
+++ src/blur.wgsl
@@ -1,13 +1,66 @@
+struct FrustumCornerDirections {
+    topLeft : vec3<f32>,
+    topRight : vec3<f32>,
+    bottomLeft : vec3<f32>,
+    bottomRight : vec3<f32>,
+}
+
+fn calculateRayDirection(uv: vec2<f32>, directions: FrustumCornerDirections) -> vec3<f32> {
+    let topInterpolated = mix(directions.topLeft, directions.topRight, uv.x);
+    let bottomInterpolated = mix(directions.bottomLeft, directions.bottomRight, uv.x);
+    let finalInterpolated = mix(topInterpolated, bottomInterpolated, uv.y);
+    return normalize(finalInterpolated);
+}
+
+fn sphNormal(pos: vec3<f32>, ce: vec3<f32>) -> vec3<f32>
+{
+    return normalize(pos-ce.xyz);
+}
+
+fn sphIntersect(ro: vec3<f32>, rd: vec3<f32>, ce: vec3<f32>, ra: f32) -> vec2<f32> {
+    let oc: vec3<f32> = ro - ce;
+    let b: f32 = dot(oc, rd);
+    let c: f32 = dot(oc, oc) - ra * ra;
+    var h: f32 = b * b - c;
+
+    if (h < 0.0) {
+        return vec2<f32>(-1.0); // no intersection
+    }
+
+    h = sqrt(h);
+
+    return vec2<f32>(-b - h, -b + h);
+}
+
 @group(0) @binding(0) var outputTex : texture_storage_2d<rgba8unorm, write>;
 @group(0) @binding(1) var<uniform> time : u32;
 @group(0) @binding(2) var<uniform> resolution : vec2<u32>;
+@group(0) @binding(3) var<uniform> frustumCornerDirections : FrustumCornerDirections;
 
 @compute @workgroup_size(1, 1, 1)
 fn main(
   @builtin(workgroup_id) WorkGroupID : vec3<u32>,
   @builtin(local_invocation_id) LocalInvocationID : vec3<u32>
 ) {
-  var red = f32(WorkGroupID.x) / f32(resolution.x) + sin(f32(time)) * 0.000001;
-  var green = f32(WorkGroupID.y) / f32(resolution.y);
-  textureStore(outputTex, WorkGroupID.xy, vec4(red,green,0,1));
+  let p = (2.0*vec2<f32>(WorkGroupID.xy)-vec2<f32>(resolution)) / vec2<f32>(resolution).y;
+  let rayOrigin = vec3(0,0,5.0);
+  let rayDirection = normalize( vec3(p,-2.0) );
+
+  var green = rayDirection.y;
+  var blue = rayDirection.z;
+
+  let timeOffset = (sin(f32(time) * 0.001) * 0.5 + 0.5) * 2.0;
+  let spherePos = vec3(0,timeOffset - 1.0,2.0);
+  let intersect = sphIntersect(rayOrigin, rayDirection, spherePos, 0.5);
+
+    var red = rayDirection.x;
+    if(intersect.x > 0.0){
+        let pos = rayOrigin + intersect.y * rayDirection;
+        let normal = -sphNormal(pos, spherePos);
+        red = normal.r;
+        green = normal.g;
+        blue = normal.b;
+    }
+
+  textureStore(outputTex, WorkGroupID.xy, vec4(red,green,blue,1));
 }
