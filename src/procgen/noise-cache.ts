d8cd99bb67c8f871dafde2fb2a80f1ce64391f89
Daniel Jackson <danieljackson97123@gmail.com>
2024-09-11 09:05:51 +0100

    remove noise cache timing

--- src/procgen/noise-cache.ts
+++ src/procgen/noise-cache.ts
@@ -27,34 +27,17 @@ export class NoiseCache {
     noiseFn: (x: number, y: number, z: number) => number,
     size: [number, number, number],
   ) {
-    const startTimestamp = performance.now();
     this.size = size;
     this.noiseFunction = noiseFn;
     this.cache = new Int16Array(size[0] * size[1] * size[2]);
-    console.debug(
-      "Noise cache",
-      size,
-      "created in",
-      (performance.now() - startTimestamp).toFixed(0),
-      "ms",
-      "size",
-      this.totalSizeBytes / 1024 ** 2,
-      "MB",
-    );
-  }
-
-  get totalSizeBytes(): number {
-    return this.cache.byteLength;
   }
 
   get([x, y, z]: [number, number, number]): number {
     const index = convert3DTo1D(this.size, [x, y, z]);
-
     const value = this.cache[index];
     if (value === 0) {
       this.cache[index] = convertFloat32ToInt16(this.noiseFunction(x, y, z));
     }
-
     return convertInt16ToFloat32(this.cache[index]);
   }
 }
