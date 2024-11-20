266652468f6e8e5aeb1798dddf6a9f15f1b0fcfc
Daniel Jackson <danieljackson97123@gmail.com>
2024-11-20 13:48:39 +0000

    remove outlines

--- src/renderer/app.ts
+++ src/renderer/app.ts
@@ -265,7 +265,7 @@ export const init = async (
     // getVignettePass(10.0),
     getLightDebugPass(device),
     fullscreenQuad(device),
-    getBoxOutlinePass(device),
+    // getBoxOutlinePass(device),
   ]);
 
   timestampLabels = computePasses.reduce((acc, val) => {
